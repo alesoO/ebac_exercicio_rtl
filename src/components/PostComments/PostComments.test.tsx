d1d2173eabcde622ce87a1e2a1edb6367221adfb
Alexandre <xxalesoaresxx@gmail.com>
2024-11-13 20:51:27 -0300

    Adição dos Testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: { value: 'Primeiro Comentario' },
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: { value: 'Segundo Comentario' },
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
